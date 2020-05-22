5482709628d1e57d63ffb28803f0eae68238029e
JellyInLaw <61371616+JellyInLaw@users.noreply.github.com>
2020-05-22 20:34:03 +0500

    + works

--- js/main.js
+++ js/main.js
@@ -7,9 +7,35 @@ burger.onclick = function () {
 };
 
 window.onscroll = function () {
-  if (window.pageYOffset > 50) {
+  if (window.pageYOffset > 80) {
     headerNav.classList.add("header__nav-onscroll");
   } else {
     headerNav.classList.remove("header__nav-onscroll");
   }
 };
+//nav
+
+let category = document.querySelectorAll(".categories__item");
+const content = document.querySelectorAll(".content");
+
+let addHidden = function () {
+  for (let cont of content) {
+    cont.classList.add("hidden");
+  }
+  for (let cat of category) {
+    cat.style.textDecoration = "underline";
+  }
+};
+
+addHidden();
+content[0].classList.remove("hidden");
+category[0].style.textDecoration = "none";
+
+for (let cat of category) {
+  cat.onclick = function () {
+    addHidden();
+    content[cat.dataset.type].classList.remove("hidden");
+    cat.style.textDecoration = "none";
+  };
+}
+//works
